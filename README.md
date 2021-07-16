52bf16c2a45c7dfdc3e2eb389e31d7cb06641075
Sam Harrison <samharrison.xg@gmail.com>
2021-07-16 17:09:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# NanoFASE model calibration
+
+This repository is a template for calibrating the [NanoFASE model](https://github.com/nerc-ceh/nanofase). It is intended for expert use and only as an example of how you may wish to calibrate the model, not as the definitive method of doing so.
+
+The Python file at [`scripts/optimize.py`](./scripts/optimize.py) is an example script of how calibration of the model can be performed, and you may wish to copy this script and modify it for you own needs. Example data is provided in the `data/` and `obs_data/` directories, which is for the Thames catchment between 2009 and 2012.
+
+## Usage
+
+To use the example [optimize.py](./scripts/optimize.py) script with the example data provided in `data/` and `obs_data`, follow these instructions. Firstly, let's clone this repo and make some directories:
+
+```bash
+$ git clone https://github.com/nerc-ceh/nanofase-calibration
+$ cd nanofase-calibration
+$ mkdir config_cache data_cache run_stdout results
+```
+Make sure the model has been compiled and that an executable resides somewhere, let's say at `/path/to/model/exe`.
+
+The Python packages required to run the optimize.py script are listed in the Conda [environment.yaml](./environment.yaml) file. Either create a new Conda environment using this:
+
+```bash
+$ conda env create -f environment.yaml
+$ conda activate nanofase-calibration
+```
+
+Or install the required packages however else you wish. Now you can run the optimize.py script, passing in the path to the calibration directory and the model executable:
+
+```bash
+(nanofase-calibration) $ cd scripts
+(nanofase-calibration) $ ./optimize.py --caldir ../ --exepath /path/to/model/exe
+```
+
+When the calibration has finished, the final calibration parameters are stored in `results/optimized_params.npy`, which is a NumPy binary file. To read this:
+
+```bash
+(nanofase-calibration) $ python
+>>> import numpy as np
+>>> params = np.load('results/optimized_params.npy')
+```
+
+### Saving intermediate results
+
+To save memory, the optimization script deletes input and output data after each iteration has completed. This means that, if the script crashes, you won't have any information on how far the iteration got. To avoid this, you can specify the `--logall` flag, which results in a `.npz` being written to the `results/` directory for each iteration (of which there could be 1000s), with the run ID hash as the file name. The disadvantage of this is that these files could take up a significant amount of disk space. To inspect these, use NumPy:
+
+```bash
+(nanofase-calibraiton) $ python
+>>> import numpy as np
+>>> result = np.load('results/<run_id_hash>.npz')
+>>> cost = result['cost']
+>>> params = result['params']
+```
+
+The `cost` variable above is the mean absolute error compute for that particular model run.
+
+Use the optimize.py script's `-h` flag to see all options:
+
+```
+usage: optimize.py [-h] [--caldir CALDIR] [--exepath EXEPATH] [--logall] [--no-logall] [--maxworkers MAXWORKERS]
+                   [--yearrange YEARRANGE YEARRANGE]
+
+Optimize the NanoFASE model
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --caldir CALDIR, -c CALDIR
+                        path to the calibration directory
+  --exepath EXEPATH, -x EXEPATH
+                        path to the model exe file
+  --logall, -l
+  --no-logall
+  --maxworkers MAXWORKERS, -mw MAXWORKERS
+                        maximum number of processes that can be used
+  --yearrange YEARRANGE YEARRANGE, -yr YEARRANGE YEARRANGE
+                        year range to run calibration for (inclusive)
+```
+
+## How the calibration works
+
+The model calibration is done on 8 parameters, which control suspended sediment concentrations. Each of these parameters is allowed to vary spatially (but not temporally), and so the total number of parameters that are being calibrated is 8 multiplied by the number of grid cells in your geographical scenario (which, as you've probably guessed, could amount to *a lot* of parameters). Suspended sediment observation data at given sampling sites is used to calibrate the model against. The general goal of the calibration is to minimise the *mean absolute error* of the simulated data vs the observation data, and this minimisation is done in parallel by the [optimparallel](https://pypi.org/project/optimparallel/) package. Internally, this package uses the [L-BFGS-B method](https://en.wikipedia.org/wiki/Limited-memory_BFGS), thereby providing a parallel version of `scipy.optimize.minimize(method='L-BFGS-B')`.
+
+Have a look at the example observation data in `obs_data/` to see what format the observation is required in. Note that a [`sites_meta.csv`](./obs_data/sites_meta.csv) file is required to provide metadata (e.g. grid references), whilst the observation data itself is provided in [`sites_obs.csv`](./obs_data/sites_obs.csv).
+
+Initial values for these parameters should be provided in the `data/x0.nc` file.
+
+## Notes and caveats
+
+- Depending on your model scenario (e.g. spatial and temporal extent), the calibration could take *a very long time*. For a desktop computer calibrating the Thames catchment for 2009 to 2012, we could be talking months or years. Two approaches are suggested to alleviate this:
+	- The optimize.py script assumes you want to vary each calibration parameter (of which there are 8) for each grid cell (of which, in the Thames, there are 634). Instead, you could assume that the calibration parameters are constant over the whole catchment, or split the catchment into subcatchments and assume the calibration parameters are constant over each of these.
+    - Find a high-performance computer with a ridiculous number of cores (>100s). The optimize.py script automatically uses the maximum number of cores that it can (or you can specify the maximum number to use by passing specifying the `--maxworkers` argument). It will still probably take a while.
+- Make sure the compiled model executable you are using was compiled with speed optimization in mind. If you are using the example Makefile, then use `make release` to achieve this. This can more than halve model run times.
+- The optimize.py script has only been tested using Bash and may need modifications to use on different systems, particularly Windows.
---
+++ scripts/optimize.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+import sys
+import os
+import argparse
+import subprocess
+import shutil
+import uuid
+import pandas as pd
+import numpy as np
+import multiprocessing
+import f90nml
+from netCDF4 import Dataset
+from optimparallel import minimize_parallel
+
+# Parse the input arguments
+parser = argparse.ArgumentParser(description='Calibrate the NanoFASE model')
+parser.add_argument('--caldir', '-c', help='path to the calibration directory', default='./')
+parser.add_argument('--exepath', '-x', help='path to the model exe file', default='./nanofase')
+parser.add_argument('--logall', '-l', dest='log_all', action='store_true')
+parser.add_argument('--no-logall', dest='log_all', action='store_true')
+parser.add_argument('--maxworkers', '-mw', help='maximum number of processes that can be used',
+                    type=int)
+parser.add_argument('--yearrange', '-yr', nargs=2, type=int, help='year range to run calibration for (inclusive)')
+parser.set_defaults(log_all=False)
+parser.set_defaults(yearrange=[2009,2012])
+args = parser.parse_args()
+cal_dir = args.caldir
+exe_path = args.exepath
+max_workers = args.maxworkers
+year_range = range(args.yearrange[0], args.yearrange[1]+1)
+
+# The parameters we wish to optimize 
+param_names = ['resuspension_alpha', 'resuspension_beta', 'sediment_transport_a', 'sediment_transport_c',
+               'deposition_alpha', 'deposition_beta', 'bank_erosion_alpha', 'bank_erosion_beta']
+
+# COST FUNCTION
+# -------------
+# Define the cost function to calibrate using. In this case, using the mean absolute error
+def cost(df):
+    mae = []
+    # Loop through all of the sites in this subcatchment
+    for i, site in df_meta.iterrows():
+        if site['full-name'] in df_obs['site_name'].str.upper().values:
+            # Get the obs date for this site
+            df_obs_site = df_obs[df_obs['site_name'].str.upper() == site['full-name']]
+            # Slice the output data to get only sim data for this site
+            df_sim = df[(df.x == site.x) & (df.y == site.y) & (df.w == site.w)]
+            # Merge the sim and obs data
+            df_merged = pd.merge(left=df_obs_site, right=df_sim, left_on='date', right_on='datetime')
+            # Calculate the RMS for this site
+            with np.errstate(divide='ignore'):
+                arr_C_spm = np.log(df_merged['C_spm(kg/m3)']*1e3).replace(-np.Inf, 0)
+                df_merged['err'] = abs(arr_C_spm - np.log(df_merged['sld_sus(mg/l)']))
+            df_merged['err'] = df_merged['err'].replace(-np.Inf, 0)
+            mae.append(df_merged['err'].mean())
+    # Average the mean absolute errors of each site
+    cost = np.array(mae).mean()
+    return cost
+
+# Read in the data to calibrate using, including site metadata
+df_meta = pd.read_csv(f'{cal_dir}obs_data/sites_meta.csv')
+df_obs = pd.read_csv(f'{cal_dir}obs_data/sites_obs.csv')
+
+# Create new config files for this run
+with open(f'{cal_dir}batch_config.nml', 'r') as f:
+    bc_template = f90nml.read(f)
+with open(f'{cal_dir}config.nml', 'r') as f:
+    config_template = f90nml.read(f)
+
+# Get a mask for the catchment shape and count the cells
+nc_subcatchment = Dataset(f'{cal_dir}data/{args.yearrange[0]}_no-emissions.nc', 'r')
+var = nc_subcatchment['flow_dir'][:,:]
+catchment_mask = var.mask
+catchment_shape = var.shape
+n_cells = var.count()
+
+
+# MODEL FUNCTION
+# --------------
+# Function to run the NanoFASE model. This is the function passed to the optimisation function
+# and takes the parameters to calibrate as a parameter, returning the cost after running the
+# model and analysing the results
+def nf_model(params):
+    # Unique run ID to keep track of parallel runs
+    run_id = uuid.uuid4().hex
+    
+    # Log this run
+    print(f'Running the model for run ID {run_id}')
+    with open(f'{cal_dir}optimize.log', 'a') as f:
+        f.write(f'Running the model for run ID {run_id}\n')
+    
+    # Copy the config templates
+    bc = bc_template.copy()
+    bc['chunks']['input_files'] = []
+    config = config_template.copy()
+    config['run']['output_hash'] = run_id
+    # Write the config file
+    config_path = f'{cal_dir}config_cache/config_{run_id}.nml'
+    with open(config_path, 'w') as f:
+        config.write(f)
+    
+    # Penalise this iteration if any params less than 0
+    if np.any(params < 0.0):
+        return 1000
+    
+    # Make a copy of the template NetCDFs to add this iteration's params to
+    for year in year_range:
+        dst_path = f'{cal_dir}data_cache/{year}_no-emissions_{run_id}.nc'
+        shutil.copy(f'{cal_dir}data/{year}_no-emissions.nc', dst_path)
+        # Update the batch config to point to this file
+        bc['chunks']['input_files'].append(dst_path)
+        
+    # Write the batch config file
+    bc_path = f'{cal_dir}config_cache/batch_config_{run_id}.nml'
+    with open(bc_path, 'w') as f:
+        bc.write(f)
+    
+    # Pull out the 1D arrays for each parameter from the params variable, then
+    # reshape to the correct grid shape and mask and add to NetCDF file
+    for i, param in enumerate(param_names):
+        param_1d = params[n_cells*i:n_cells*i+n_cells]
+        param_2d = np.ma.masked_array(np.empty(catchment_shape), mask=catchment_mask)
+        # Reshape into 2D arrays, taking into account the mask
+        j = 0
+        for i, _ in np.ndenumerate(param_2d):
+            if ~catchment_mask[i]:
+                param_2d[i] = param_1d[j]
+                j = j + 1
+        # Now add this variable to the NetCDF file, placing a copy in the cache
+        for year in year_range:
+            # Then create the new variables
+            nc = Dataset(f'{cal_dir}data_cache/{year}_no-emissions_{run_id}.nc', 'r+')
+            var = nc.createVariable(param, datatype=float, dimensions=('y','x'))
+            var[:] = param_2d
+            nc.close()
+            
+    # Run the model and save the output to the run_stdout dir
+    # with open(f'{cal_dir}run_stdout/run_{run_id}.out', 'w') as file:
+    #     subprocess.run([
+    #         exe_path,
+    #         config_path,
+    #         bc_path,
+    #     ], check=True, text=True, stdout=file, stderr=file)
+
+    # # Remove the output file - we only need it if there was an error, and 
+    # # an error will have already trigerred an exception
+    # os.remove(f'{cal_dir}run_stdout/run_{run_id}.out')
+    
+    # # Evaluate the output and return the cost
+    # df_out = pd.read_csv(f'{cal_dir}output/output_water{run_id}.csv',
+    #                      parse_dates=['datetime'])
+    # Return the cost, calculated from obs and sim data
+    # cost_ = cost(df_out)
+    cost_ = 42
+
+    # If we're logging the params and costs of all iterations, then do so 
+    if args.log_all:
+        with open(f'{cal_dir}results/{run_id}.npz', 'wb') as f:
+            np.savez_compressed(f, cost=cost_, params=params0)
+
+    # Remove the NetCDF files for this run
+    for year in year_range:
+        os.remove(f'{cal_dir}data_cache/{year}_no-emissions_{run_id}.nc')
+            
+    # Also remove the output file - we can recreate this once we've got the calibrated params
+    if os.path.isfile(f'{cal_dir}output/output_water{run_id}.csv'):
+        os.remove(f'{cal_dir}output/output_water{run_id}.csv')
+        os.remove(f'{cal_dir}output/output_sediment{run_id}.csv')
+        os.remove(f'{cal_dir}output/output_soil{run_id}.csv')
+        os.remove(f'{cal_dir}output/summary{run_id}.md')
+    
+    with open(f'{cal_dir}optimize.log', 'a') as f:
+        f.write(f'Cost for {run_id}: {cost_}\n')
+    return cost_
+
+
+# OPTIMIZATION
+# ------------
+# Perform the optimization, using the x0.nc NetCDF file for initial guesses at
+# the parameters
+data0 = f'{cal_dir}data/x0.nc'
+params0 = np.empty((0,))
+# For each parameter, get it from the NetCDF file, flatten it and then add to the params0 array
+for param in param_names:
+    nc = Dataset(data0)
+    var = nc[param][:,:]
+    flat = np.ravel(var[~var.mask])
+    params0 = np.concatenate((params0, flat))
+
+# Actually do the optimization
+result = minimize_parallel(fun=nf_model, x0=params0,
+                           parallel={ 'max_workers': max_workers, 'verbose': True})
+
+# Write the result to the optimize log
+with open(f'{cal_dir}optimize.log', 'a') as f:
+    f.write(f'Final cost (MAE): {result.fun}\n')
+    f.write(f'Number of evaluations: {result.nfev}\n')
+    f.write(f'Number of iterations: {result.nit}\n')
+    f.write(f'Successful? {result.success}')
+
+# Write params to file, so we can re-run the optimized result
+with open(f'{cal_dir}results/optimized_params.npy', 'wb') as f:
+    np.save(f, result.x)
